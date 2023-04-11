738b7ada0ec8ca6c6b8c300584a5a1c28eed6189
张宇Never_Lie <12711057+zhang-lies@user.noreply.gitee.com>
2023-04-11 04:50:31 +0000

    update AstroLib/Include/AsOrbitParam_2023.h. 老师，这是张赛师兄的程序，我帮他提交的。
    Signed-off-by: 张宇Never_Lie <12711057+zhang-lies@user.noreply.gitee.com>

--- AstroLib/Include/AsOrbitParam_2023.h
+++ AstroLib/Include/AsOrbitParam_2023.h
@@ -21,3 +21,20 @@ bool	AsModOrbElemToCart(
 	double gm,
 	CCoord& pos,
 	CCoord& vel);
+
+//********************************************************************
+/// cartesian state element to modified orbit element
+/// @Author	Zhang Sai
+/// @Date	2023.04.03
+/// @Input
+/// @Param	pos		位置
+/// @Param	vel		速度
+/// @Param	gm		中心体引力常数
+/// @Output
+/// @Param	modOrb	m_I			[0, pi]
+///					m_RAAN		[0, 2pi]
+///					m_ArgPeri	[0, 2pi]
+///					m_TrueA		[0, 2pi]
+/// @Return			true=成功; false=输入错误
+//********************************************************************
+bool	AsCartToModOrbElem(const CCoord3& pos, const CCoord3& vel, double gm, CModOrbElem& modOrb);
