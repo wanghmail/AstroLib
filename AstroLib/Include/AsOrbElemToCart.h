3680405704fc35eac246e6c74089301624b2e87e
guanyuming <12705544+guanyuming@user.noreply.gitee.com>
2023-04-13 06:04:05 +0000

    轨道六根数转位置速度头文件
    Signed-off-by: guanyuming <12705544+guanyuming@user.noreply.gitee.com>

---
+++ AstroLib/Include/AsOrbElemToCart.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <stdlib.h>
+#include "AstroLib.h"
+#include <cmath>
+using namespace std;
+bool AsOrbElemToCart(const COrbElem& Orb, double m1, double m2, CCoord& cartPos, CCoord & cartVel);
